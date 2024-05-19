20dd64a4a4eba471726a6429c274dea098333f51
Luca <duvet86@gmail.com>
2024-05-19 13:39:58 +0800

    first commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+
 import {
   Links,
   Meta,
@@ -5,6 +7,16 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+import stylesheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
+
+export const meta: MetaFunction = () => {
+  return [{ title: "TMS" }, { name: "description", content: "TMS Sadleirs" }];
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
