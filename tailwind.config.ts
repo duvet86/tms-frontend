b8cf10e11c3a1712708ac14a9e8fff10db8952d8
Luca <duvet86@gmail.com>
2024-05-19 14:41:34 +0800

    working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography, daisyui],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 } satisfies Config;
