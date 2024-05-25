831f4d319c876f36f2f9bba158473047319ba370
Luca <duvet86@gmail.com>
2024-05-25 20:56:44 +0800

    working

--- app/.server/auth.ts
+++ app/.server/auth.ts
@@ -11,8 +11,6 @@ import { OAuth2Strategy } from "remix-auth-oauth2";
 
 import { sessionStorage } from "./session";
 
-const SCOPES = ["openid", "profile", "email", "offline_access"];
-
 export interface MicrosoftStrategyOptions
   extends Omit<
     OAuth2StrategyOptions,
@@ -34,10 +32,7 @@ interface MicrosoftExtraParams extends Record<string, string | number> {
 
 export interface MicrosoftProfile extends OAuth2Profile {
   id: string;
-  userPrincipalName: string;
   displayName: string;
-  givenName: string | null;
-  surname: string | null;
 }
 
 interface User {
@@ -77,6 +72,7 @@ export class MicrosoftStrategy<User> extends OAuth2Strategy<
         redirectURI,
         authorizationEndpoint: `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize`,
         tokenEndpoint: `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`,
+        tokenRevocationEndpoint: `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/logout`,
         authenticateWith: "request_body",
         codeChallengeMethod: "S256",
         scopes,
@@ -95,7 +91,9 @@ export class MicrosoftStrategy<User> extends OAuth2Strategy<
   protected async userProfile(
     tokens: TokenResponseBody & MicrosoftExtraParams
   ): Promise<MicrosoftProfile> {
-    const resp = await fetch("https://graph.microsoft.com/v1.0/me", {
+    console.log(tokens);
+
+    const resp = await fetch(`${process.env.API_BASE_URL}/v1/api/me`, {
       headers: {
         Authorization: `Bearer ${tokens.access_token}`,
       },
@@ -103,7 +101,10 @@ export class MicrosoftStrategy<User> extends OAuth2Strategy<
 
     const user = await resp.json();
 
-    return user;
+    return {
+      ...user,
+      provider: "oauth2",
+    };
   }
 }
 
@@ -111,12 +112,23 @@ const microsoftStrategy = new MicrosoftStrategy(
   {
     clientId: process.env.CLIENT_ID!,
     clientSecret: process.env.CLIENT_SECRET!,
-    redirectURI: "http://localhost:5173/auth/microsoft/callback",
+    redirectURI: process.env.AUTH_REDIRECT_URL!,
     tenantId: process.env.TENANT_ID,
-    scopes: SCOPES,
+    scopes: [
+      // "openid",
+      // "profile",
+      // "email",
+      // "offline_access",
+      `api://${process.env.API_ID}/Read.User`,
+    ],
     prompt: "login",
   },
-  async ({ tokens, profile }) => ({ tokens, profile })
+  async ({ tokens, profile }) => {
+    console.log(tokens);
+    console.log(profile);
+
+    return { tokens, profile };
+  }
 );
 
 authenticator.use(microsoftStrategy);
